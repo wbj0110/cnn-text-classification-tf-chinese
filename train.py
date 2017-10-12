bccf4159584f2081e8d01dc9b6412d979a30160a
soledede.weng <wengbenjue@163.com>
2017-10-12 15:42:13 +0800

    cnn text classfy

--- train.py
+++ train.py
@@ -20,14 +20,17 @@ tf.flags.DEFINE_string("negative_data_file", "./data/rt-polaritydata/rt-polarity
 
 # Model Hyperparameters
 tf.flags.DEFINE_integer("embedding_dim", 128, "Dimensionality of character embedding (default: 128)")
-tf.flags.DEFINE_string("filter_sizes", "1,2,3,4,5,6,8", "Comma-separated filter sizes (default: '1,2,3,4,5,6,8')")
+tf.flags.DEFINE_string("filter_sizes", "2,3,3,3,3,3,4", "Comma-separated filter sizes (default: '1,2,3,4,5,6,8')")
 tf.flags.DEFINE_string("num_filters", "50,100,150,150,200,200,200", "Number of filters per filter size (default: 50,100,150,150,200,200,200)")
+#tf.flags.DEFINE_string("filter_sizes", "3", "Comma-separated filter sizes (default: '1,2,3,4,5,6,8')")
+#tf.flags.DEFINE_string("num_filters", "300", "Number of filters per filter size (default: 50,100,150,150,200,200,200)")
+
 tf.flags.DEFINE_float("dropout_keep_prob", 0.5, "Dropout keep probability (default: 0.5)")
 tf.flags.DEFINE_float("l2_reg_lambda", 0.8, "L2 regularization lambda (default: 0.0)")
 
 # Training parameters
 tf.flags.DEFINE_integer("batch_size", 64, "Batch Size (default: 64)")
-tf.flags.DEFINE_integer("num_epochs", 200, "Number of training epochs (default: 200)")
+tf.flags.DEFINE_integer("num_epochs", 100, "Number of training epochs (default: 200)")
 tf.flags.DEFINE_integer("evaluate_every", 100, "Evaluate model on dev set after this many steps (default: 100)")
 tf.flags.DEFINE_integer("checkpoint_every", 59, "Save model after this many steps (default: 100)")
 tf.flags.DEFINE_integer("num_checkpoints", 5, "Number of checkpoints to store (default: 5)")
@@ -71,8 +74,8 @@ y_shuffled = y[shuffle_indices]
 # TODO: This is very crude, should use cross-validation
 #dev_sample_index = -1 * int(FLAGS.dev_sample_percentage * float(len(y)))
 
-x_train, x_dev = x_shuffled[:-300], x_shuffled[-300:]
-y_train, y_dev = y_shuffled[:-300], y_shuffled[-300:]
+x_train, x_dev = x_shuffled[:-500], x_shuffled[500:]
+y_train, y_dev = y_shuffled[:-500], y_shuffled[500:]
 sequence_length = x_train.shape[1]
 print("Vocabulary Size: {:d}".format(len(vocabulary)))
 print("Train/Dev split: {:d}/{:d}".format(len(y_train), len(y_dev)))
